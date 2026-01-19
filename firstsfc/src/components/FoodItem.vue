c947156e0abae4a1ba11885deed9b83cce02c571
Rafael Laurence E. Martinez <remartinez@student.apc.edu.ph>
2026-01-19 21:31:36 +0300

    Update FoodItem.vue
    Updated the FoodItem component to include styling and changed the name and message to reflect a single fruit.

--- firstsfc/src/components/FoodItem.vue
+++ firstsfc/src/components/FoodItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="Strawberry">
     <h2>{{ name }}</h2>
     <p>{{ message }}</p>
   </div>
@@ -9,12 +9,20 @@
 export default {
   data() {
     return {
-      name: 'Strawberry and Avocado',
-      message: 'I like Strawberry and Avocado'
+      name: 'Strawberry',
+      message: 'I like Strawberry'
     }
   }
 };
 </script>
 
-<style></style>
+<style>
+  .Strawberry {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: red;
+  }
 
+</style>
