9496e29660896735ac5efc12d06070c6fceb9047
Rafael Laurence E. Martinez <remartinez@student.apc.edu.ph>
2026-01-19 21:32:21 +0300

    Create FoodItem2.vue

---
+++ firstsfc/src/components/FoodItem2.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="Avocado">
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Avocado',
+      message: 'I like Avocado'
+    }
+  }
+};
+</script>
+
+<style>
+  .durian {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: rgb(213, 237, 76);
+  }
+
+
+</style>
