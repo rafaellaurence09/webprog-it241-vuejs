63e8e25babe6cabd5a6887a15aad74831d086899
Rafael Laurence E. Martinez <remartinez@student.apc.edu.ph>
2026-01-30 01:38:03 +0300

    changes

--- firstsfc/src/components/CommentForm.vue
+++ firstsfc/src/components/CommentForm.vue
@@ -20,7 +20,7 @@
   
   <script setup>
   import { ref } from 'vue';
-  import { supabase } from '../lib/supabaseClient'
+  import { supabase } from '../lib/supabaseClient-js'
 
   
   const name = ref('');
